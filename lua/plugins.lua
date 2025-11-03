d9354dd1f35c355165e0b907ca3d255cbcb9374d
devEducaua <schokito4@gmail.com>
2025-11-03 18:45:59 -0300

    refactor: centralize plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,15 +20,12 @@ bootstrap_pckr()
 local pckr = require("pckr")
 
 pckr.add{
-    "~/proj/oradark.nvim",
+    { "~/proj/oradark.nvim", branch = "minimal" },
 
-    "windwp/nvim-autopairs",
+    "nvim-mini/mini.pairs",
     "numToStr/Comment.nvim",
-
-    {
-        "L3MON4D3/LuaSnip",
-        config = require("configs.luasnip")
-    },
+    "williamboman/mason.nvim",
+    "L3MON4D3/LuaSnip",
 
     {
         "saghen/blink.cmp",
@@ -57,7 +54,6 @@ pckr.add{
         ---@module 'oil'
         ---@type oil.SetupOpts
         requires = { "nvim-tree/nvim-web-devicons" },
-        config = require("configs.oil")
     },
 
     {
@@ -83,13 +79,6 @@ pckr.add{
     },
 
     {
-        "williamboman/mason.nvim",
-        config = function()
-	    require("mason").setup()
-	end
-    },
-
-    {
         "nvim-treesitter/nvim-treesitter",
         run = ":TSUpdate",
         config = function ()
@@ -108,3 +97,69 @@ pckr.add{
         end
     }
 }
+
+require("snippets")
+require('mini.pairs').setup({})
+require("mason").setup()
+
+local commands = require("commands")
+
+require("oil").setup({
+    default_file_explorer = true,
+    skip_confirm_for_simple_edits = true,
+    watch_for_changes = true,
+    view_options = {
+        show_hidden = true,
+    },
+    columns = {
+        "icon",
+        "type",
+        "permissions",
+        "size",
+        "mtime",
+    }, keymaps = {
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g,"] = { "actions.cd", mode = "n" },
+
+        ["<space>l"] = {
+            desc = "download template license",
+            callback = function ()
+                vim.fn.feedkeys(":License ")
+            end
+        },
+
+        ["<space>h"] = {
+            desc = "pass file to command",
+            callback = function ()
+               local name = commands.get_path(require("oil").get_cursor_entry().name)
+               local keys = "q:" .. "iCmd " .. name .. "<esc>Bi"
+               keys = vim.api.nvim_replace_termcodes(keys, true, false, true)
+               vim.api.nvim_feedkeys(keys, 'n', false)
+            end
+        },
+
+        ["<space>r"] = {
+            desc = "run file executables or Makefiles",
+            callback = function ()
+                local name = require("oil").get_cursor_entry().name
+                commands.exec_by_name(name)
+            end
+        },
+
+        ["<space>ga"] = {
+            desc = "stage file on git",
+            callback = function ()
+                local file = commands.get_path(require("oil").get_cursor_entry().name)
+                vim.fn.feedkeys(":Cmd git add " .. get_path(file), "nt")
+            end
+        },
+
+        ["<space>grm"] = {
+            desc = "remove file from git",
+            callback = function ()
+                local file = commands.get_path(require("oil").get_cursor_entry().name)
+                vim.fn.feedkeys(":Cmd git rm " .. get_path(file), "nt")
+            end
+        },
+    },
+})
