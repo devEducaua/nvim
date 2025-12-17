89c535cd88ac5b40df73165ddc5697e7229cf6eb
devEducaua <schokito4@gmail.com>
2025-12-17 19:04:45 -0300

    fix: rename snippets class to export

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -3,10 +3,9 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
-local extras = require("luasnip.extras")
 
 ls.add_snippets("typescript", {
-    s("class", {
+    s("export", {
         t("export default class "), i(1, "name"), t(" {"),
         t({ "", "\t"}), i(2),
         t({ "", "}" }),
