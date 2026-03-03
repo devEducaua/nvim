51a6e3d7f9929c1b9e3ee1b2d351d0b9135b4b21
devEducaua <schokito4@gmail.com>
2026-03-02 22:45:30 -0300

    fix: remove unused snippets

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -4,36 +4,6 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
-ls.add_snippets("typescript", {
-    s("exp", {
-        t("export default class "), i(1, "name"), t(" {"),
-        t({ "", "\t"}), i(2),
-        t({ "", "}" }),
-    }),
-
-    s("method", {
-        t({"", "\t"}), i(1), t("() {"),
-        t({ "", "\t"}), i(2),
-        t({ "", "}" }),
-    }),
-
-    s("amethod", {
-        t({"", "async "}), i(1), t("() {"),
-        t({ "", "\t"}), i(2),
-        t({ "", "}" }),
-    }),
-})
-
-ls.add_snippets("c", {
-    s("main", {
-        t("#include <stdio.h>"),
-        t({ "", "", "" }),
-        t("int main() {"),
-        t({ "", "\t"}), i(0),
-        t({ "", '\treturn 0;'}), t({ "", "}" }), 
-    })
-})
-
 ls.add_snippets("html", {
     s("html", {
         t("<DOCTYPE html>"),
@@ -71,24 +41,5 @@ ls.add_snippets("jsonc", {
         t{"", '\t],'},
         t{"", '\t"include": ["src"]'},
         t({" ", "}"})
-    })
-})
-
-ls.add_snippets("tex", {
-    s("basic", {
-        t({"\\documentclass{article}"}),
-        t({"", ""}),
-        t({"", "\\author{Indu}"}),
-        t({"", "\\title{"}), i(1), t("}"),
-        t({"", ""}),
-        t({"", "\\begin{document}"}),
-        t({"", ""}),
-        t({"", "\\end{document}"}),
-    })
-})
-
-ls.add_snippets("markdown", {
-    s("a", {
-        t("["), i(1), t("]"), t("("), i(2), t(")")
     })
 })
